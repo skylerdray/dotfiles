bc96b3bfc2417fecd44c8684a4ec3656fbc7ee74
Skyler Dray <skylerdray@gmail.com>
2016-09-18 09:12:00 -0400

    "this is the one"

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -44,8 +44,8 @@ end
 beautiful.init("/usr/share/awesome/themes/default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "x-terminal-emulator"
-editor = os.getenv("EDITOR") or "editor"
+terminal = "urxvt"
+editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
 -- Default modkey.
@@ -83,11 +83,14 @@ end
 
 -- {{{ Tags
 -- Define a tag table which hold all screen tags.
-tags = {}
+tags = {
+   names = { "www", "rdc", "chats", "4", "5"},
+   layout = { layouts[8], layouts[11], layouts[2], layouts[1], layouts[1] }
+}
 for s = 1, screen.count() do
-    -- Each screen has its own tag table.
-    tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, layouts[1])
+   tags[s] = awful.tag(tags.names, s, tags.layout)
 end
+
 -- }}}
 
 -- {{{ Menu
